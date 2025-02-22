928cca072a219bd46f31131cb047f37bb2a6a688
RushanthanG <70675057+RushanthanGunalan@users.noreply.github.com>
2025-02-22 17:43:56 +0530

    Changed Config Settings in config.js file

--- tests/pomtest.spec.js
+++ tests/pomtest.spec.js
@@ -1,1 +1,15 @@
 import { expect, test } from "@playwright/test";
+import LoginPage from "./../Pages/LoginPage";
+
+let loginPage;
+
+test.describe("Login Tests", () => {
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+  });
+
+  test("Logging in with Credentials", async () => {
+    await loginPage.navigate();
+    await loginPage.loging("tomsmith", "SuperSecretPassword!");
+  });
+});
